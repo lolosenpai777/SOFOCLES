7eb589bd905133e788c15f237b27e28628add72b
Berskboi <lolosenpai777@gmail.com>
2026-07-15 19:20:46 -0500

    maqueta

---
+++ Sofocles/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <-- 1. Importas el plugin
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // <-- 2. Lo agregas aquí
+  ],
+})
+
+
+
